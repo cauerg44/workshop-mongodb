b344833e9be82964143e5673226dad23ba649cc0
Caue Garcia <cauerg7@outlook.com>
2024-04-23 16:34:45 -0300

    workshop project created.

---
+++ src/main/java/com/devsuperior/workshopmongo/WorkshopmongoDevApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoDevApplication.class, args);
+	}
+
+}
